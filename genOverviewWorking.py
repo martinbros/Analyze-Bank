8e3348bf7790f648c6e1bec9c235a3d231938120
martinbros <67989270+martinbros@users.noreply.github.com>
2024-09-29 13:38:20 -0700

    small edits

--- genOverviewWorking.py
+++ genOverviewWorking.py
@@ -59,7 +59,7 @@ allYears = ["2019Q1_2019-01-08_2019-04-02\\transactionsCatagorized.csv",
 			"2024_Q3_working\\transactionsCatagorized.csv",]
 
 accountDF = pd.DataFrame()
-for path in paths2024:
+for path in allYears:
 	accountDF = accountDF.append(pd.read_csv(path), sort=False)
 
 
--- graphBank.py
+++ graphBank.py
@@ -15,6 +15,7 @@ accounts = {"wfAutographJanSept.csv": "ONLINE TRANSFER REF #[A-Z0-9]* TO WELLS F
 autoDF = removeRows("wfAutographJanSept.csv", ["ONLINE PAYMENT THANK YOU"])
 platDF = removeRows("wfPlatinumJanSept.csv", ["ONLINE PAYMENT THANK YOU"])
 chckDF = removeRows("wfCheckingJanSept.csv", ["ONLINE TRANSFER REF #[A-Z0-9]* TO WELLS FARGO AUTOGRAPH", "ONLINE TRANSFER REF #[A-Z0-9]* TO PLATINUM"])
+# Need to save info of what was removed
 ckAccount = pd.concat([chckDF, platDF, autoDF])
 
 print(ckAccount)
@@ -24,7 +25,7 @@ newCatDf.to_csv("masterCatagories.csv", index=False)
 
 
 # Use the following lines below to generate a quarterly report
-saveP, graphP, catCkAcct = sliceDfCreateDir2(accountDF, 2024, 3)
+saveP, graphP, catCkAcct = sliceDfCreateDir2(accountDF, 2024, 1)
 catCkAcct.to_csv(saveP + "\\transactionsCatagorized.csv", index=False)
 newCatDf.to_csv(saveP + "\\updatedCatagories.csv", index=False)
 saveLineGraphs(catCkAcct, "catagory", graphP)
